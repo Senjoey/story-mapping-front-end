2262ca274b7026e00b576bcf917db6653a951ca1
Senjoey <141250122@smail.nju.edu.cn>
2019-01-13 15:19:33 +0800

    [dev]新增和删除故事地图UI

--- src/pages/StoryMapping/StoryMappingOverview.js
+++ src/pages/StoryMapping/StoryMappingOverview.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { Card, List, Button, Icon, Menu, Dropdown, Modal} from 'antd';
+import { Card, List, Button, Icon, Menu, Dropdown, Modal, message, Form, Input} from 'antd';
 import styles from './storyMappingOverview.less';
 import {translateTimestampToTime} from '../../util/DateUtil'
 
-export default class StoryMappingOverviewPage extends Component {
+class StoryMappingOverviewPage extends Component {
     constructor() {
         super();
         this.state = {
@@ -39,7 +39,8 @@ export default class StoryMappingOverviewPage extends Component {
                     createMapList = res.content.createMapList.map(item =>{
                         return {
                             title: item.title,
-                            createTime: translateTimestampToTime(new Date(item.createTime))
+                            createTime: translateTimestampToTime(new Date(item.createTime)),
+                            id: item.id
                         }
                     });
                     memberMapList = res.content.memberMapList;
@@ -56,43 +57,47 @@ export default class StoryMappingOverviewPage extends Component {
     handleOk(){
         let createMapList = this.state.createMapList;
         this.setState({
-            ModalText: '正在添加用户故事地图...',
             confirmLoading: true,
         });
-        fetch('http://172.19.240.8:8080/map', {
-            method: 'POST',
-            mode: "cors",
-            headers: new Headers({
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + localStorage.getItem('token')
-            }),
-            //TODO title获取用户输入
-            body: JSON.stringify({'title': '用户故事地图1'})
-        }).then((res)=>{
-            return res.json()
-        }).then((res)=>{
-            console.log(res);
-            createMapList.push({
-                title: res.content.title,
-                createTime: translateTimestampToTime(res.content.createTime)
-            });
-            this.setState({
-                createMapList: createMapList,
-                visible: false,
-                confirmLoading: false,
-            })
-        }).catch((err)=>{
-            console.log('error: ', err)
+        this.props.form.validateFields((err, values) => {
+            if(!err) {
+                console.log('Received values of form: ', values);
+                fetch('http://172.19.240.8:8080/map', {
+                    method: 'POST',
+                    mode: "cors",
+                    headers: new Headers({
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Bearer ' + localStorage.getItem('token')
+                    }),
+                    body: JSON.stringify(values)
+                }).then((res)=>{
+                    return res.json()
+                }).then((res)=>{
+                    console.log(res);
+                    createMapList.push({
+                        title: res.content.title,
+                        createTime: translateTimestampToTime(res.content.createTime),
+                        id: res.content.id
+                    });
+                    this.setState({
+                        createMapList: createMapList,
+                        visible: false,
+                        confirmLoading: false,
+                    })
+                }).catch((err)=>{
+                    console.log('error: ', err)
+                });
+            }
         });
+
     }
     handleCancel() {
         this.setState({
             visible: false,
         });
     }
-    handleMapDelete() {
-        //TODO 获取当前的mapID
-        fetch('http://172.19.240.8:8080/map?mapId=14', {
+    handleMapDelete(mapID) {
+        fetch(`http://172.19.240.8:8080/map?mapId=${mapID}`, {
             method: 'DELETE',
             mode: "cors",
             headers: new Headers({
@@ -110,21 +115,26 @@ export default class StoryMappingOverviewPage extends Component {
             console.log('error: ', err)
         });
     }
+    handleMapEdit(mapID) {
+        //TODO 获取当前的mapID
+        message.success(`当前ID: ${mapID}`);
+    }
     render() {
-        const menu = (
-            <Menu className={styles.menu}>
-                <Menu.Item ><Icon type="edit" />修改</Menu.Item>
-                <Menu.Item onClick={this.handleMapDelete.bind(this)}><Icon type="minus" />删除</Menu.Item>
-            </Menu>
-        );
-        const dropdownGroup = (
-            <span className={styles.iconGroup}>
-                <Dropdown overlay={menu} placement="bottomRight">
-                    <Icon type="ellipsis" />
-                </Dropdown>
-            </span>
-        );
-        const { visible, confirmLoading, ModalText } = this.state;
+        // const menu = (
+        //     <Menu className={styles.menu}>
+        //         <Menu.Item onClick={()=>console.log()}><Icon type="edit" />修改</Menu.Item>
+        //         <Menu.Item onClick={this.handleMapDelete.bind(this)}><Icon type="minus" />删除</Menu.Item>
+        //     </Menu>
+        // );
+        // const dropdownGroup = (
+        //     <span className={styles.iconGroup}>
+        //         <Dropdown overlay={menu} placement="bottomRight">
+        //             <Icon type="ellipsis" />
+        //         </Dropdown>
+        //     </span>
+        // );
+        const { visible, confirmLoading} = this.state;
+        const { getFieldDecorator } = this.props.form;
         return(
             <div style={{maxWidth: "1200px", margin:"0 auto"}}>
                 <div className={styles.cardList}>
@@ -138,10 +148,22 @@ export default class StoryMappingOverviewPage extends Component {
                                     <Card
                                         hoverable
                                         title={item.title}
-                                        extra={dropdownGroup}
+                                        // extra={dropdownGroup}
+                                        extra={
+                                            <span className={styles.iconGroup}>
+                                                <Dropdown placement="bottomRight" overlay={
+                                                    <Menu className={styles.menu}>
+                                                        <Menu.Item onClick={this.handleMapEdit.bind(this, item.id)}><Icon type="edit" />修改</Menu.Item>
+                                                        <Menu.Item onClick={this.handleMapDelete.bind(this, item.id)}><Icon type="minus" />删除</Menu.Item>
+                                                    </Menu>
+                                                }>
+                                                    <Icon type="ellipsis" />
+                                                </Dropdown>
+                                            </span>
+                                        }
                                     >
-                                        <p>创建时间：</p>
-                                        <p>{item.createTime}</p>
+                                        <p>创建时间: {item.createTime}</p>
+                                        <p>ID: {item.id}</p>
                                     </Card>
                                 </List.Item>
                             ) : (
@@ -150,13 +172,21 @@ export default class StoryMappingOverviewPage extends Component {
                                         <Icon type="plus" /> 新增故事地图
                                     </Button>
                                     <Modal
-                                        title="Title"
+                                        title="新增故事地图"
                                         visible={visible}
                                         onOk={this.handleOk.bind(this)}
                                         confirmLoading={confirmLoading}
                                         onCancel={this.handleCancel.bind(this)}
                                     >
-                                        <p>{ModalText}</p>
+                                        <Form>
+                                            <Form.Item label="标题">
+                                                {getFieldDecorator('title', {
+                                                    rules: [{ required: true }],
+                                                })(
+                                                    <Input />
+                                                )}
+                                            </Form.Item>
+                                        </Form>
                                     </Modal>
                                 </List.Item>
                             )
@@ -167,3 +197,4 @@ export default class StoryMappingOverviewPage extends Component {
         );
     }
 }
+export default Form.create()(StoryMappingOverviewPage);
